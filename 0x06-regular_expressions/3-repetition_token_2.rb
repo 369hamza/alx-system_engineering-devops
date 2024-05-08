60f46590c40773a3a57b20e7b59e3c004ab69211
369hamza <el.ouardi.hamza369@gmail.com>
2024-05-07 17:32:54 -0700

    fixing again

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 
 def match_school(input)
-    regex = /hb(t{1,4})n/
+    regex = /hb(t{1,5})n/
 
     match_result = input.match(regex)
 
     puts match_result ? match_result[0] : ''
 end
+
+match_pattern(ARGV[0])
