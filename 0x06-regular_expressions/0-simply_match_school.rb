2f1f6c327872be75512d485f48b184495614d845
369hamza <el.ouardi.hamza369@gmail.com>
2024-05-07 18:42:52 -0700

    fixing again

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+def match_school(input)
+    regex = /School/
+
+    match_result = input.match(regex)
+
+    puts match_result ? match_result[0] : ''
+end
+
+match_school(ARGV[0])
